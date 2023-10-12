c9444e0674fbb21fba6dcd5fb4ee146d3dc42cde
virgil2019 <danten0402@gmail.com>
2023-10-13 00:49:44 +0800

    Update

--- src/contracts/BTCChain.js
+++ src/contracts/BTCChain.js
@@ -10,7 +10,13 @@ class EVMChainDeployer {
         this.contracts = {};
     }
 
-    launchBitcoinBackend() {
+    launchBitcoinBackend(chainInfo) {
+        console.log('chainInfo', chainInfo)
+        let cfg = `export default {provider: "${chainInfo.rpc}",}`;
+        fs.writeFileSync(
+            config.get('submodules.bitcoinBackendPath') + 'config/config.ts',
+            cfg
+          );
         spawn('npx', ['ts-node', 'index.ts'], {
             cwd: config.get('submodules.bitcoinBackendPath'),
           });
@@ -18,7 +24,7 @@ class EVMChainDeployer {
 
     deployOmniverse(chainInfo) {
         // Launch bitcoin backend
-        this.launchBitcoinBackend();
+        this.launchBitcoinBackend(chainInfo);
     }
 }
 
